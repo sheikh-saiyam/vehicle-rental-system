db4554d09109efb25d6fce6d121c059131a00a3d
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-12-09 19:59:11 +0600

    fix: update .gitignore and correct bookings route import in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from "express";
 import initDB from "./config/db";
 import logger from "./middleware/logger";
 import { authRoutes } from "./modules/auth/auth.routes";
+import { bookingsRoutes } from "./modules/booking/booking.route";
 import { userRoutes } from "./modules/user/user.routes";
 import { vehicleRoutes } from "./modules/vehicle/vehicle.routes";
-import { bookingsRoutes } from "./modules/booking/booking.route";
 
 const app = express();
 
@@ -23,7 +23,7 @@ app.get("/", async (req: Request, res: Response) => {
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/vehicles", vehicleRoutes);
-app.use("/api/v1/bookings", bookingsRoutes)
+app.use("/api/v1/bookings", bookingsRoutes);
 
 //* NOT FOUND ROUTE
 app.use((req: Request, res: Response) => {
